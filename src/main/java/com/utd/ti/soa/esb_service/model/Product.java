ae895e4ea478abc347de3b2b62d1bd33e9405c50
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-04-01 19:36:59 -0600

    Create Product.java

---
+++ src/main/java/com/utd/ti/soa/esb_service/model/Product.java
@@ -0,0 +1,15 @@
+package com.utd.ti.soa.esb_service.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String nombre;
+    private String precio;
+    private String stock;
+    private String descripcion;
+    private String imagen;
+    private String categoria;
+}
